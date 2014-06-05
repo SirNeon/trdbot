a3c42f6a5a4b4fe32d915c7858322169d404e1f9
SirNeon618 <SirNeon618@users.noreply.github.com>
2014-06-04 20:51:26 -0400

    Damn near rewrote the whole thing.
    Code's not working yet, as the thread fetching seems to not be working correctly. Will get on that tomorrow.

--- trdbot.py
+++ trdbot.py
@@ -1,88 +1,246 @@
-# these imports are of bad style
-# native modules get declared first
-# don't use commas unless you're importing multiple
-# things from ONE module
-# 3rd party modules get declared last
-import sqlite3
-import praw
-import sys, os
+# fixed bad style and added stuff for new features
+from datetime import datetime
+import logging
+from sys import exit, stderr
 from time import sleep
+import praw
+from requests import HTTPError
 
-# this is weird and might not work; why not use tuples in a list?
-SUBREDDIT_PROFILE = {'subredditdrama': ['SRD', 10],
-                   'drama': ['MILEY', 3]}
-#insert a comma after each new penultimate line if you modify the above to have more subs.
-#the number is the vote threshold for xposting. An SRD post must have 10 to trigger xposting.
-print '  Logging in as Mirrorbot'
-r = praw.Reddit(user_agent='/r/MirrorNetwork xposting bot instance')
-r.login('username', 'password')
 
-# I think doing all of this in multiple functions in a class
-# would make it easier to maintain as well as add flexibility
-def main():
-    trd = r.get_subreddit('mirrornetworktest') #this is the subreddit it will POST INTO
-    multireddit = r.get_subreddit('+'.join(SUBREDDIT_PROFILE.keys()))
-    # why?
-    path_to_db = os.path.abspath(os.path.dirname(sys.argv[0]))
-    path_to_db = os.path.join(path_to_db, 'examplebot.db')
-    con = sqlite3.connect(path_to_db)
-    con.row_factory = sqlite3.Row
-    cur = con.cursor()
-    # check for new posts
-    cur.execute("SELECT * FROM data WHERE type = 'last_submission'")
-    row = cur.fetchone()
-    last_submission = int(row['value'])
-    new_last_submission = None
-    print '  Checking for new drama submissions to cross-post'
-#    for post in multireddit.get_new_by_date(limit=None):        ##commented out, outdated praw feature, changed below
-    for post in multireddit.get_new(limit=100): #limit=None was scraping lots more than it needed to, this reduces CPU use.
-        # passing these absurdly long names to variable to conserve
-        # space would be nice
-        if int(post.created_utc) <= last_submission or post.score < SUBREDDIT_PROFILE[post.subreddit.display_name.lower()][1]:
-            break
+# make it a class to improve maintainability and flexibility
+class trdbot(object):
+
+
+    def __init__(self):
+        """
+        Initialize the bot with some basic info.
+        """
+
+        # optional logging
+        self.errorLogging = True
+
+        self.userAgent = "/r/MirrorNetwork xposting bot"
+
+        # list of subreddits to crawl
+        self.subredditList = set(["SubredditDrama", "Drama"])
+
+        # subreddit flairs
+        self.flairList = [
+                {"subredditdrama": "SRD"}, {"drama": "MILEY"}
+            ]
+
+        # list of stuff already done
+        self.alreadyDone = set()
+
+        # post to this subreddit
+        self.post_to = "mirrornetworktest"
+
+        # scrape no more than this number of threads
+        self.scrapLimit = 25
+
+
+    def login(self, username, password):
+        """
+        Login to the bot's Reddit account. Give it the username
+        and the password.
+        """
+
+        self.client = praw.Reddit(user_agent=self.userAgent)
+        print "Logging in as %s..." % username
+
+        self.client.login(username, password)
+        print "Login was successful."
+
+
+    def check_new(self, submissionID):
+        """
+        Checks if a submission has already been posted. Give
+        it the submission's ID.
+        """
+
+        if submissionID in self.alreadyDone:
+            return False
+
+        else:
+            return True
+
+
+    def check_post(self, submission):
+        """
+        Checks to see if the submission meets the necessary
+        criteria to be posted.
+        """
+
         try:
-            #print '  Cross-posting "{0}" from /r/{1}'.format(post.title, post.subreddit.display_name).encode('ascii', 'ignore')
-            if (post.is_self):
-                submission = trd.submit(title=post.title, text=post.selftext.replace('np.reddit.com', 'www.reddit.com'))
-            else:
-                submission = trd.submit(title=post.title, url=post.url.replace('np.reddit.com', 'www.reddit.com'))
-            if not new_last_submission:
-                new_last_submission = int(post.created_utc)
-                # for what this bot does it probably won't matter,
-                # but this is vulnerable to SQLInjection I think
-                # unless format() is different from %s
-                cur.execute("UPDATE data "
-                            "SET value = '{0}' "
-                            "WHERE type = 'last_submission'".format(new_last_submission))
-                con.commit()
-            submission.set_flair('[{0}]'.format(SUBREDDIT_PROFILE[post.subreddit.display_name.lower()][0]))
-            submission.add_comment('[Comments in source subreddit]({0})'.format(post.permalink))
-        # could use more HTTPError handling
-        except praw.errors.APIException:
-            pass
-if __name__ == '__main__':
-     while 1:
+            subName = submission.subreddit.lower()
+            subScore = submission.score 
+
+        except AttributeError:
+            print "Failed to collect necessary data."
+            return False
+
+        # SRD threads need at least +10 karma
+        if subName == "subredditdrama":
+            if subScore >= 10:
+                return True
+
+        # Drama threads need at least +3 karma
+        elif subName == "drama":
+            if subScore >= 3:
+                return True
+
+        # everything else needs at least +7 karma
+        else:
+            if subScore >= 7:
+                return True
+
+        return False
+
+
+    def submit_content(self, submission):
+        """
+        This will submit the post to Reddit. Give it a tuple with
+        the post title and then post body in it.
+        """
+
+        title = submission.title
+
+        if(submission.is_self):
+            # swap out normal reddit links for np links
+            text = submission.selftext.replace("www.reddit.com", "np.reddit.com")
+
+        else:
+            text = submission.url.replace("www.reddit.com", "np.reddit.com")
+
+        mySubreddit = self.client.get_subreddit(self.post_to)
+
+        mySubreddit.submit(title, text)
+
+
+    def log_err(self, error):
+        """
+        This is for logging errors.
+        """
+
+        if(self.errorLogging):
+            logging.basicConfig(
+                filename="trdbot_logerr.log", filemode='a', 
+                format="%(asctime)s\nIn %(filename)s "
+                "(%(funcName)s:%(lineno)s): %(message)s", 
+                datefmt="%Y-%m-%d %H:%M", level=logging.DEBUG, 
+                stream=stderr
+            )
+            
+            logging.debug(str(error) + "\n\n")
+
+
+if __name__ == "__main__":
+    myBot = trdbot()
+
+    username = ""
+    password = ""
+
+    # set this to False to turn off logging
+    # I don't recommend that though
+    myBot.errorLogging = True
+
+    try:
+        myBot.login(username, password)
+
+    except (praw.errors.InvalidUser, praw.errors.InvalidUserPass, HTTPError) as e:
+        print e
+        myBot.log_err(e)
+        exit(1)
+
+    while True:
+        # keep the list from getting too big
+        if len(myBot.alreadyDone) == 1000:
+            for i, element in enumerate(myBot.alreadyDone):
+                if i < 900:
+                    myBot.alreadyDone.remove(element)
+
         try:
-            # wat; one infinite loop is enough
-            while 1:
-                # one try/except should be enough as well
-                try:
-                    main()
-                    print 'try...sleeping...'
-                    sleep(60)
-                except Exception, e:
-                    print str(e)
-                    print 'exception...retrying...'
-                    main()
-                    print 'except...sleeping...'
-                    sleep(60)
-        except Exception, e:
-            print str(e)
-            # yet another? These will prevent the first loop from
-            # iterating, so it may as well not exist
-            while 1:
-                print 'MAJOR exception...retrying...'
-                main()
-                print 'MAJOR except...sleeping...'
-                sleep(60)
-#this try/except thing is my own, but i have NO IDEA what im doing. It still crashed and terminates despite the except/retry thing. Any ideas?
+            # iterate through the list of subreddits to scan
+            for subreddit in myBot.subredditList:
+                # scan the new queue
+                submissions = myBot.client.get_subreddit(subreddit).get_new(limit=myBot.scrapLimit)
+
+                for i, submission in enumerate(submissions):
+                    try:
+                        print "Scanning thread (%d / %d)..." % (i + 1, myBot.scrapLimit)
+                        # make sure the bot hasn't already done
+                        # this thread before
+                        newPost = myBot.check_new(submission.id)
+                        
+                        # get the name of the sub where the
+                        # thread was posted to originally
+                        subName = submission.subreddit.lower()
+
+                    except AttributeError:
+                        continue
+
+                    if(newPost):
+                        print "Checking if post is worthy..."
+                        # make sure the post meets the criteria
+                        # necessary to xpost it
+                        goodPost = self.check_post(submission)
+
+                    if(goodPost):
+                        myBot.alreadyDone.add(submission.id)
+
+                        # keep trying to submit the post until
+                        # either it's successful or you get an 
+                        # error besides HTTPError
+                        while True:
+                            try:
+                                print "Submitting thread..."
+                                post = myBot.submit_content(submission)
+                                break
+
+                            except HTTPError, e:
+                                print str(e) + "Sleeping...trying again..."
+                                myBot.log_err(e)
+                                sleep(60)
+
+                            # keyboard interrupt will skip post 
+                            # instead of killing the bot
+                            except KeyboardInterrupt:
+                                print "Skipping post..."
+                                break
+
+                            except Exception, e:
+                                print str(e) + "Skipping thread..."
+                                myBot.log_err(e)
+                                break
+
+                    if(post):
+                        # try to assign flair 3 times
+                        # could have it try more, but that
+                        # doesn't seem worth the time
+                        for i in range(0, 2):
+                            try:
+                                print "Setting flair..."
+                                post.set_flair(myBot.flairList[subName])
+                                print "Adding link to source..."
+                                post.add_comment("[Comments in source subreddit](%s)", submission.permalink)
+                                break
+
+                            except HTTPError, e:
+                                print str(e) + "Sleeping... trying again..."
+                                sleep(60)
+                                continue
+
+                            except KeyboardInterrupt:
+                                print "Skipping flair/commenting..."
+                                break
+
+                            except Exception, e:
+                                print str(e) + "Skipping..."
+                                sleep(60)
+                                break
+
+        except (HTTPError, Exception) as e:
+            print str(e) + "Sleeping... trying again..."
+            myBot.log_err(e)
+            sleep(60)
+            continue
